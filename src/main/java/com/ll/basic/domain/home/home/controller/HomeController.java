b7db734e67060bb5b11a7650a345df34a746818e
Nicholas070 <kid0807@naver.com>
2024-12-11 16:20:55 +0900

    write step 1

--- src/main/java/com/ll/basic/domain/home/home/controller/HomeController.java
+++ src/main/java/com/ll/basic/domain/home/home/controller/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 public class HomeController {
@@ -107,8 +108,8 @@ public class HomeController {
     @ResponseBody
     public List<Article> getArticles() {
         return List.of(
-                Article.builder().title("제목1").body("내용1").build(),
-                Article.builder().title("제목2").body("내용2").build()
+                Article.builder().id(1).title("제목1").body("내용1").build(),
+                Article.builder().id(2).title("제목2").body("내용2").build()
         );
     }
 
@@ -116,9 +117,27 @@ public class HomeController {
     @ResponseBody
     public Map<String, Article> articleMap() {
         return Map.of(
-                "article1", Article.builder().title("제목1").body("내용1").build(),
-                "article2", Article.builder().title("제목2").body("내용2").build()
+                "article1", Article.builder().id(1).title("제목1").body("내용1").build(),
+                "article2", Article.builder().id(2).title("제목2").body("내용2").build()
         );
+    }
+
+    @GetMapping("/articleList.html")
+    @ResponseBody
+    public String getArticlesDotHtml() {
+        Article article1 = Article.builder().id(1).title("제목1").body("내용1").build();
+        Article article2 = Article.builder().id(2).title("제목2").body("내용2").build();
+        Article article3 = Article.builder().id(3).title("제목3").body("내용3").build();
+        Article article4 = Article.builder().id(4).title("제목4").body("내용4").build();
+
+        List<Article> articles = List.of(article1, article2, article3, article4);
+
+        String lis = articles
+                .stream()
+                .map(article -> "<li>%d번 / %s</li>".formatted(article.getId(), article.getTitle()))
+                .collect(Collectors.joining("\n"));
+
+        return "<ul style=\"font-weight: bold; color: red;\">\n" + lis + "\n</ul>";
     }
 }
 
